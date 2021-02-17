1d51e4425dc9f1f2f2fb9ffb06808bf268570c63
Austin Yates <67457437+AustinStoneui@users.noreply.github.com>
2021-02-17 13:57:59 -0500

    DejaMe landing page

---
+++ src/components/Cards.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these Amazing products!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Children'
+              path='/services'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Women'
+              path='/services'
+            />
+            <CardItem
+              src='https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGljdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Men'
+              path='/services'
+            />
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
